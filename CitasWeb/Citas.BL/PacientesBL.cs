da57a71f21cc5cb24cf91c86463031963a1c1714
Evelin Membreño <evelinmem15@gmail.com>
2020-02-24 16:47:10 -0600

    bien

---
+++ CitasWeb/Citas.BL/PacientesBL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admon.BL
+{
+    public class PacientesBL
+    {
+        Contexto _contexto;
+        public List<Paciente> ListadePacientes { get; set; }
+
+        public PacientesBL()
+        {
+            _contexto = new Contexto();
+            ListadePacientes = new List<Paciente>();
+        }
+
+        public List<Paciente> ObtenerPaciente()
+        {
+            ListadePacientes = _contexto.Pacientes.ToList();
+            return ListadePacientes;
+        }
+    }
+}
